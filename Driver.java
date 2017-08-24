42ecb71c287a9147832cc48452e8673e498d0bb6
Xing Liu <xingliu@utexas.edu>
2017-08-23 20:34:51 -0500

    fix: update Driver.java with 4 MapReduce jobs

--- Driver.java
+++ Driver.java
@@ -4,16 +4,24 @@ public class Driver {
 		
 		preProcessByUser preProcessByUser = new preProcessByUser();
 		preProcessByItem preProcessByItem = new preProcessByItem();
+		preProcessBaseline preProcessBaseline = new preProcessBaseline();
+		normalizeRating normalizeRating = new normalizeRating();
 		
 		String rawInput = args[0];
 		String byUserOutputDir = args[1];
 		String byItemOutputDir = args[2];
+		String baselineOutputDir = args[3];
+		String normalizeOutputDir = args[4];
 		
 		String[] path1 = {rawInput, byUserOutputDir};
 		String[] path2 = {rawInput, byItemOutputDir};
+		String[] path3 = {byItemOutputDir, baselineOutputDir};
+		String[] path4 = {baselineOutputDir, byItemOutputDir, byUserOutputDir, normalizeOutputDir};
 		
 		preProcessByUser.main(path1);
 		preProcessByItem.main(path2);
+		preProcessBaseline.main(path3);
+		normalizeRating.main(path4);
 	}
 
 }
