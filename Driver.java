fdd1bf8b59cabfc6c5e82a9f3ebeaa04649ad6eb
Xing Liu <xingliu@utexas.edu>
2017-08-23 23:07:21 -0500

    fix: update Driver.java with 6 mapreduce jobs

--- Driver.java
+++ Driver.java
@@ -7,6 +7,7 @@ public class Driver {
 		preProcessBaseline preProcessBaseline = new preProcessBaseline();
 		normalizeRating normalizeRating = new normalizeRating();
 		cooccurance cooccurance = new cooccurance();
+		similarity similarity = new similarity();
 		
 		String rawInput = args[0];
 		String byUserOutputDir = args[1];
@@ -14,18 +15,21 @@ public class Driver {
 		String baselineOutputDir = args[3];
 		String normalizeOutputDir = args[4];
 		String cooccuranceOutputDir = args[5];
+		String similarityOutputDir = args[6];
 		
 		String[] path1 = {rawInput, byUserOutputDir};
 		String[] path2 = {rawInput, byItemOutputDir};
 		String[] path3 = {byItemOutputDir, baselineOutputDir};
 		String[] path4 = {baselineOutputDir, byItemOutputDir, byUserOutputDir, normalizeOutputDir};
 		String[] path5 = {byUserOutputDir, cooccuranceOutputDir};
+		String[] path6 = {cooccuranceOutputDir, similarityOutputDir};
 		
 		preProcessByUser.main(path1);
 		preProcessByItem.main(path2);
 		preProcessBaseline.main(path3);
 		normalizeRating.main(path4);
 		cooccurance.main(path5);
+		similarity.main(path6);
 	}
 
 }
